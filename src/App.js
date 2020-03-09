19b5646c466e5d875970a1f62c2729cd3204359d
SHINTSY <545656660@qq.com>
2020-03-09 22:52:50 +0800

    add: add recommend components(list, banner)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { HashRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { renderRoutes } from 'react-router-config'
+import store from './store'
+import routes from './routes'
+import { GlobalStyle } from './style'
+import { IconStyle } from './assets/iconfont/iconfont'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <Router>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </Router>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
